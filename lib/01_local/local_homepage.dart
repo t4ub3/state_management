6818a88a0d197677bb114fde26e81bc9d321752c
Jonas N. <jn.jonasneumann@googlemail.com>
2026-05-28 13:42:31 +0200

    local state

--- lib/01_local/local_homepage.dart
+++ lib/01_local/local_homepage.dart
@@ -8,11 +8,56 @@ class LocalHomepage extends StatefulWidget {
 }
 
 class _LocalHomepageState extends State<LocalHomepage> {
-  int _counter = 0;
+  int _counterTopLeft = 0;
+  int _counterTopRight = 0;
+  int _counterBottomLeft = 0;
+  int _counterBottomRight = 0;
 
-  void _incrementCounter() {
+  void _incrementCounterTopLeft() {
     setState(() {
-      _counter++;
+      _counterTopLeft++;
+    });
+  }
+
+  void _incrementCounterTopRight() {
+    setState(() {
+      _counterTopRight++;
+    });
+  }
+
+  void _incrementCounterBottomLeft() {
+    setState(() {
+      _counterBottomLeft++;
+    });
+  }
+
+  void _incrementCounterBottomRight() {
+    setState(() {
+      _counterBottomRight++;
+    });
+  }
+
+  void _decrementCounterTopLeft() {
+    setState(() {
+      _counterTopLeft--;
+    });
+  }
+
+  void _decrementCounterTopRight() {
+    setState(() {
+      _counterTopRight--;
+    });
+  }
+
+  void _decrementCounterBottomLeft() {
+    setState(() {
+      _counterBottomLeft--;
+    });
+  }
+
+  void _decrementCounterBottomRight() {
+    setState(() {
+      _counterBottomRight--;
     });
   }
 
@@ -21,22 +66,104 @@ class _LocalHomepageState extends State<LocalHomepage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Titel'),
+        title: Text('Overengineered Counter'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
-          ],
-        ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
+        padding: EdgeInsets.all(20),
+        children: [
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 20,
+              children: [
+                IconButton(
+                  color: Colors.green[400],
+                  onPressed: _incrementCounterBottomRight,
+                  icon: Icon(Icons.arrow_upward),
+                ),
+                Text(
+                  _counterTopLeft.toString(),
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight(500)),
+                ),
+                IconButton(
+                  color: Colors.red[300],
+                  onPressed: _decrementCounterBottomRight,
+                  icon: Icon(Icons.arrow_downward),
+                ),
+              ],
+            ),
+          ),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 20,
+              children: [
+                IconButton(
+                  color: Colors.green[400],
+                  onPressed: _incrementCounterBottomLeft,
+                  icon: Icon(Icons.arrow_upward),
+                ),
+                Text(
+                  _counterTopRight.toString(),
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight(500)),
+                ),
+                IconButton(
+                  color: Colors.red[300],
+                  onPressed: _decrementCounterBottomLeft,
+                  icon: Icon(Icons.arrow_downward),
+                ),
+              ],
+            ),
+          ),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 20,
+              children: [
+                IconButton(
+                  color: Colors.green[400],
+                  onPressed: _incrementCounterTopRight,
+                  icon: Icon(Icons.arrow_upward),
+                ),
+                Text(
+                  _counterBottomLeft.toString(),
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight(500)),
+                ),
+                IconButton(
+                  color: Colors.red[300],
+                  onPressed: _decrementCounterTopRight,
+                  icon: Icon(Icons.arrow_downward),
+                ),
+              ],
+            ),
+          ),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 20,
+              children: [
+                IconButton(
+                  color: Colors.green[400],
+                  onPressed: _incrementCounterTopLeft,
+                  icon: Icon(Icons.arrow_upward),
+                ),
+                Text(
+                  _counterBottomRight.toString(),
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight(500)),
+                ),
+                IconButton(
+                  color: Colors.red[300],
+                  onPressed: _decrementCounterTopLeft,
+                  icon: Icon(Icons.arrow_downward),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
