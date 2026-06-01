56bc887627709cbe0a51f077a1d4560714a780fc
Jonas N. <jn.jonasneumann@googlemail.com>
2026-06-01 16:17:38 +0200

    clean up

--- lib/04_global/counter_provider.dart
+++ lib/04_global/counter_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CounterTopLeftNotifier extends Notifier<int> {
+class CounterNotifier extends Notifier<int> {
   @override
   int build() => 0; // initial value
 
@@ -15,65 +15,18 @@ class CounterTopLeftNotifier extends Notifier<int> {
   void set(int value) => state = value;
 }
 
-final counterTopLeftProvider = NotifierProvider<CounterTopLeftNotifier, int>(
-  CounterTopLeftNotifier.new,
+final counterTopLeftProvider = NotifierProvider<CounterNotifier, int>(
+  CounterNotifier.new,
 );
 
-class CounterTopRightNotifier extends Notifier<int> {
-  @override
-  int build() => 0; // initial value
-
-  void increment() => state++;
-  void decrement() {
-    if (state > 0) {
-      state--;
-    }
-  }
-
-  void reset() => state = 0;
-  void set(int value) => state = value;
-}
-
-final counterTopRightProvider = NotifierProvider<CounterTopRightNotifier, int>(
-  CounterTopRightNotifier.new,
+final counterTopRightProvider = NotifierProvider<CounterNotifier, int>(
+  CounterNotifier.new,
 );
 
-class CounterBottomLeftNotifier extends Notifier<int> {
-  @override
-  int build() => 0; // initial value
+final counterBottomLeftProvider = NotifierProvider<CounterNotifier, int>(
+  CounterNotifier.new,
+);
 
-  void increment() => state++;
-  void decrement() {
-    if (state > 0) {
-      state--;
-    }
-  }
-
-  void reset() => state = 0;
-  void set(int value) => state = value;
-}
-
-final counterBottomLeftProvider =
-    NotifierProvider<CounterBottomLeftNotifier, int>(
-      CounterBottomLeftNotifier.new,
-    );
-
-class CounterBottomRightNotifier extends Notifier<int> {
-  @override
-  int build() => 0; // initial value
-
-  void increment() => state++;
-  void decrement() {
-    if (state > 0) {
-      state--;
-    }
-  }
-
-  void reset() => state = 0;
-  void set(int value) => state = value;
-}
-
-final counterBottomRightProvider =
-    NotifierProvider<CounterBottomRightNotifier, int>(
-      CounterBottomRightNotifier.new,
-    );
+final counterBottomRightProvider = NotifierProvider<CounterNotifier, int>(
+  CounterNotifier.new,
+);
